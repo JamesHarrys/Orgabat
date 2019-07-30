781d2eefb85fcc60a3dc667b6dfb3263806725ed
James Harrys <bissickj@gmail.com>
2019-07-30 16:52:47 +0200

    Fix main.js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -7,3 +7,15 @@ toggle.innerHTML = `
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('active');
 }
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
